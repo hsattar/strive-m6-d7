39fdc37747685d24f8852b9a57b2065de1e397c7
hsattar <39243545+hsattar@users.noreply.github.com>
2022-01-11 14:00:36 +0000

    added blog schema

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 import express from 'express'
 import cors from 'cors'
-import Mongoose from 'mongoose'
+import mongoose from 'mongoose'
+
+const { PORT, DB_CONNECTION } = process.env
 
 const server = express()
 
 server.use(express.json())
 server.use(cors())
 
+mongoose.connect(DB_CONNECTION)
+
+mongoose.connection.on('connected', () => {
+    console.log('DB Connected')
+    server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+})
+
+mongoose.connection.on('error', err => console.log('DB Connection Failed', err))
