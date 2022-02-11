63873eb90fddcec15405d939d7883b7b9c2d032d
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-11 14:13:45 +0000

    fixed oauth error

--- src/utils/oauth.ts
+++ src/utils/oauth.ts
@@ -1,39 +1,40 @@
 import passport from "passport"
-import GoogleStrategy from 'passport-google-oauth20'
-import UserModal from "../db-models/userSchema"
-import { createNewTokens } from '../utils/jwt'
+import GoogleStrategy from "passport-google-oauth20"
+import UsersModel from "../db-models/userSchema"
+import { createNewTokens } from "./jwt"
 
-const { GOOGLE_OAUTH_CLIENT, GOOGLE_OAUTH_SECRET, BE_URL } = process.env
+const { GOOGLE_OAUTH_ID, GOOGLE_OAUTH_SECRET, BE_URL } = process.env
 
 const googleStrategy = new GoogleStrategy({
-    clientID: GOOGLE_OAUTH_CLIENT,
+    clientID: GOOGLE_OAUTH_ID,
     clientSecret: GOOGLE_OAUTH_SECRET,
     callbackURL: `${BE_URL}/users/googleRedirect`,
   }, async (accessToken: string, refreshToken: string, profile: any, passportNext: any) => {
     try {
-        console.log(profile)
-        const user = await UserModal.findOne({ googleId: profile.id })
-        if (user) {
-            const tokens = await createNewTokens(user)
-            passportNext(null, { tokens })
-        } else {
-            const newUser = new UserModal({
-                firstName: profile.name.givenName,
-                lastName: profile.name.familyName,
-                email: profile.emails[0].value,
-                googleId: profile.id
-            })
-            const savedUser = await newUser.save()
-            const tokens = await createNewTokens(savedUser)
-            passportNext(null, { tokens })
-        }
+      console.log("PROFILE: ", profile)
+      const user = await UsersModel.findOne({ googleId: profile.id })
+      if (user) {
+        const tokens = await createNewTokens(user)
+        passportNext(null, { tokens })
+      } else {
+        const newUser = new UsersModel({
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          googleId: profile.id,
+        })
+        const savedUser = await newUser.save()
+        const tokens = await createNewTokens(savedUser)
+        passportNext(null, { tokens })
+      }
     } catch (error) {
-        passportNext(error)
+      passportNext(error)
     }
-})
+  }
+)
 
-passport.serializeUser((data, passportNext) => {
-    passportNext(null, data)
+passport.serializeUser(function (data, passportNext) {
+  passportNext(null, data)
 })
 
 export default googleStrategy
